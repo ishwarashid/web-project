86c8c5678d18a66f0678ebf0b40314e341151751
Azan <azanrashid26@gmail.com>
2023-12-03 14:23:43 +0500

    Add files via upload

--- admin-panel/handle-login.php
+++ admin-panel/handle-login.php
@@ -1,33 +1,25 @@
-<?php
-
-include("../database/database-helper.php");
-include("includes/session.php");
-
-
-if(isset($_POST["login"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-
-
-
-    $query = "SELECT * FROM admin WHERE username = '$username' AND password = '$password';";
-    $result = mysqli_query($conn, $query) or die("Query failed");
-    if(mysqli_num_rows($result) >0 ){
-
-        $row = mysqli_fetch_assoc($result);
-
-            $_SESSION["USERNAME"] = $row["username"];
-            $_SESSION["SESSION_ID"] = $row["id"];
-
-
-            header('Location: index.php');
-
-
+<?php 
+    if (isset($_POST['login'])) {
+        include("../database/database-helper.php");
+        if (empty($_POST['username'])  || empty($_POST['password'])) {
+            echo "<div class='alert alert-danger'>All fields must be entered</div>";
+            die(); 
+        }else{
+            $username = mysqli_real_escape_string($conn,$_POST['username']);
+            $password = $_POST['password'];
+            $sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password';";
+            $result = mysqli_query($conn,$sql) or die("Query Failed");
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    session_start();
+                    $_SESSION['USERNAME'] = $row['username'];
+                    $_SESSION['SESSION_ID'] = $row['id'];
+                    header("Location: index.php");
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Username and Password are not matched.</div>";
+            }
+        }
     }
 
-} else {
-    header('Location: page-register.php');
-}
-
-?>
+    ?>
--- admin-panel/index.php
+++ admin-panel/index.php
@@ -213,7 +213,7 @@
 						Terms & Conditions</a>
                     </li>
 					<li class="nav-item">
-                        <a class="nav-link" href="front-login.php">
+                        <a class="nav-link" href="handle-logout.php">
 						<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1" class="svg-main-icon">
 							<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
 								<rect x="0" y="0" width="24" height="24"/>
